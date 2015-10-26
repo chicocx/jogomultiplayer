95223ca8405ff17a9962b0439b0ac9a49941e0ca
Francisco Calaça Xavier <chicocx@gmail.com>
2015-10-25 22:12:27 -0200

    teste

---
+++ JogoMultiplayer/SalaEspera.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace JogoMultiplayer
+{
+    partial class SalaEspera
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.jogadores = new System.Windows.Forms.ListBox();
+            this.txtApelido = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.btnConectar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // jogadores
+            // 
+            this.jogadores.FormattingEnabled = true;
+            this.jogadores.Location = new System.Drawing.Point(12, 64);
+            this.jogadores.Name = "jogadores";
+            this.jogadores.Size = new System.Drawing.Size(395, 355);
+            this.jogadores.TabIndex = 0;
+            // 
+            // txtApelido
+            // 
+            this.txtApelido.Location = new System.Drawing.Point(83, 9);
+            this.txtApelido.Name = "txtApelido";
+            this.txtApelido.Size = new System.Drawing.Size(210, 20);
+            this.txtApelido.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(42, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Apelido";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 39);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Nome";
+            // 
+            // txtNome
+            // 
+            this.txtNome.Location = new System.Drawing.Point(83, 39);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(210, 20);
+            this.txtNome.TabIndex = 4;
+            // 
+            // btnConectar
+            // 
+            this.btnConectar.Location = new System.Drawing.Point(300, 1);
+            this.btnConectar.Name = "btnConectar";
+            this.btnConectar.Size = new System.Drawing.Size(107, 58);
+            this.btnConectar.TabIndex = 5;
+            this.btnConectar.Text = "Conectar";
+            this.btnConectar.UseVisualStyleBackColor = true;
+            this.btnConectar.Click += new System.EventHandler(this.btnConectar_Click);
+            // 
+            // SalaEspera
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(561, 428);
+            this.Controls.Add(this.btnConectar);
+            this.Controls.Add(this.txtNome);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtApelido);
+            this.Controls.Add(this.jogadores);
+            this.Name = "SalaEspera";
+            this.Text = "Sala de Espera";
+            this.Load += new System.EventHandler(this.SalaEspera_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox jogadores;
+        private System.Windows.Forms.TextBox txtApelido;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.Button btnConectar;
+    }
+}
+
---
+++ JogoMultiplayer/SalaEspera.cs
@@ -0,0 +1,214 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace JogoMultiplayer
+{
+    public partial class SalaEspera : Form
+    {
+        private Dictionary<string, Jogador> jogadoresMapa = new Dictionary<string, Jogador>();
+
+        private Boolean conectado;
+
+        public SalaEspera()
+        {
+            InitializeComponent();
+        }
+
+        private void tratarMsg02(byte[] retorno)
+        {
+
+            string stringData = Encoding.ASCII.GetString(retorno);
+
+            int tipoMsg = Int32.Parse(stringData.Substring(0, 2));
+            int tamanhoMsg = Int32.Parse(stringData.Substring(2, 3));
+            String mensagem = stringData.Substring(5, tamanhoMsg - 5);
+
+            String[] dados = mensagem.Split('|');
+
+            if (!jogadoresMapa.ContainsKey(dados[0]))
+            {
+                Jogador jog = new Jogador();
+                jog.apelido = dados[0];
+                jog.nome = dados[1];
+                jog.confirmado = true;
+                jogadoresMapa.Add(jog.apelido, jog);
+            }
+            else
+            {
+                Jogador jog = jogadoresMapa[dados[0]];
+                jog.confirmado = true;
+            }
+
+        }
+
+        private int getIdxLista(String nick)
+        {
+            int idx = 0;
+            foreach(String txt in jogadores.Items)
+            {
+                if (txt.Contains(nick))
+                {
+                    return idx;
+                }
+                idx++;
+            }
+            return -1;
+        }
+
+        private void SalaEspera_Load(object sender, EventArgs e)
+        {
+            Thread threadAtualizadoraUsuarios = new Thread(()=> {
+                while (true)
+                {
+                    List<String> apelidosRemover = new List<String>();
+
+                    this.Invoke((MethodInvoker)(() => jogadores.Items.Clear()));
+                    foreach (var item in jogadoresMapa)
+                    {
+                        Jogador jog = item.Value;
+                        var diffInSeconds = (DateTime.Now - jog.momento).TotalSeconds;
+                        String texto = jog.apelido + " - " + jog.nome + (jog.confirmado ? " (CONFIRMADO)" : "");
+                        if (diffInSeconds > 10)
+                        {
+                            apelidosRemover.Add(jog.apelido);
+                        }
+                        else if (!jogadores.Items.Contains(texto))
+                        {
+                            this.Invoke((MethodInvoker)(() => jogadores.Items.Add(texto)));
+                        }
+
+                    }
+                    foreach(string appRev in apelidosRemover)
+                    {
+                        jogadoresMapa.Remove(appRev);
+                    }
+                    Thread.Sleep(1000);
+                }
+
+            });
+            threadAtualizadoraUsuarios.Start();
+
+            Thread threadLeitoraBroadcast = new Thread(() => {
+                Socket sock = new Socket(AddressFamily.InterNetwork,
+                                      SocketType.Dgram, ProtocolType.Udp);
+                IPEndPoint iep = new IPEndPoint(IPAddress.Any, 20152);
+                sock.Bind(iep);
+                EndPoint ep = (EndPoint)iep;
+
+                while (true)
+                {
+                    byte[] data = new byte[1024];
+                    int recv = sock.ReceiveFrom(data, ref ep);
+                    string stringData = Encoding.ASCII.GetString(data, 0, recv);
+
+                    int tipoMsg = Int32.Parse(stringData.Substring(0, 2));
+                    int tamanhoMsg = Int32.Parse(stringData.Substring(2, 3));
+                    String mensagem = stringData.Substring(5, tamanhoMsg - 5);
+
+                    String[] dados = mensagem.Split('|');
+
+                    switch (tipoMsg)
+                    {
+                        case 1:
+                            if (!jogadoresMapa.ContainsKey(dados[0]))
+                            {
+                                Jogador jog = new Jogador();
+                                jog.apelido = dados[0];
+                                jog.nome = dados[1];
+                                jogadoresMapa.Add(jog.apelido, jog);
+                            }
+                            else
+                            {
+                                Jogador jog = jogadoresMapa[dados[0]];
+                                jog.momento = DateTime.Now;
+                            }
+
+                            if (conectado)
+                            {
+                                String nick = txtApelido.Text;
+                                String nome = txtNome.Text;
+                                int tamanho = 6 + nick.Length + nome.Length;
+                                String mensagemEnvio = String.Format("02{0:D3}{1}|{2}", tamanho, nick, nome);
+                                byte[] bytes = Encoding.ASCII.GetBytes(mensagemEnvio);
+                                sock.SendTo(bytes, 0, bytes.Length, SocketFlags.None, ep);
+                                
+                            }
+                            break;
+                        case 2:
+                            tratarMsg02(data);
+                            break;
+                    }
+
+                }
+
+                sock.Close();
+
+
+            });
+            threadLeitoraBroadcast.Start();
+
+        }
+
+        private void jogadores_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnConectar_Click(object sender, EventArgs e)
+        {
+            if (!conectado)
+            {
+                conectado = true;
+                Thread threadEnvioBroadcast = new Thread(() => {
+                    while (conectado)
+                    {
+                        String nick = txtApelido.Text;
+                        String nome = txtNome.Text;
+                        int tamanho = 6 + nick.Length + nome.Length;
+                        String mensagem = String.Format("01{0:D3}{1}|{2}", tamanho, nick, nome);
+                        UdpClient client = new UdpClient();
+                        IPEndPoint ip = new IPEndPoint(IPAddress.Broadcast, 20152);
+                        byte[] bytes = Encoding.ASCII.GetBytes(mensagem);
+                        client.Send(bytes, bytes.Length, ip);
+
+                        byte[] retorno = client.Receive(ref ip);
+                        tratarMsg02(retorno);
+
+                        client.Close();
+                        Thread.Sleep(1000);
+                    }
+                });
+                threadEnvioBroadcast.Start();
+                btnConectar.Text = "Desconectar";
+            }
+            else
+            {
+                conectado = false;
+                btnConectar.Text = "Conectar";
+            }
+        }
+    }
+
+    class Jogador
+    {
+        public String apelido;
+
+        public String nome;
+
+        public DateTime momento = DateTime.Now;
+
+        public Boolean confirmado;
+
+    }
+
+}
